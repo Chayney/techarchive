3c79c76b588cd21d05c7ba341df3061bf3ff8c1a
Chayney <chunchunmaru33333@gmail.com>
2026-07-07 16:25:07 +0900

    バックエンドの構築

--- backend/node/src/routes/bookmark.route.ts
+++ backend/node/src/routes/bookmark.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createBookmarkArticlesHandler, deleteBookmarkArticlesHandler, getBookmarkArticlesHandler } from "../controller/bookmark.controller";
+import { authMiddleware } from "../middleware/auth";
 
 export const bookmarkRouter = Router();
 
-bookmarkRouter.get("/bookmarks", getBookmarkArticlesHandler);
-bookmarkRouter.post("/bookmark", createBookmarkArticlesHandler);
-bookmarkRouter.delete("/bookmark", deleteBookmarkArticlesHandler);
+bookmarkRouter.get("/bookmarks", authMiddleware, getBookmarkArticlesHandler);
+bookmarkRouter.post("/bookmark", authMiddleware, createBookmarkArticlesHandler);
+bookmarkRouter.delete("/bookmark", authMiddleware, deleteBookmarkArticlesHandler);
--- backend/node/src/routes/folder.route.ts
+++ backend/node/src/routes/folder.route.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { createFolderHandler, createFolderTagPlatformsHandler, getFeedOptionsHandler, getFolderArticlesHandler, getFolderHandler } from "../controller/folder.controller";
+import { createFolderHandler, createFolderTagPlatformsHandler, deleteFolderHandler, getFeedOptionsHandler, getFolderArticlesHandler, getFolderHandler, updateFolderHandler } from "../controller/folder.controller";
 
 export const folderRouter = Router();
 
 // グルーピングする親フォルダ
 folderRouter.get("/folders", getFolderHandler);
 folderRouter.post("/folder", createFolderHandler);
+folderRouter.put("/folder/:id", updateFolderHandler);
+folderRouter.delete("/folder/:id", deleteFolderHandler);
 
 // 親フォルダにまとめられた各集約フォルダ
 // React/QiitaやReact/Zennのようなリストを作る
 folderRouter.get("/tags/platforms", getFeedOptionsHandler);
 folderRouter.post("/tags/platforms", createFolderTagPlatformsHandler);
 
+// 子フォルダの記事一覧取得
 folderRouter.get("/tags/platforms/:tagPlatformId", getFolderArticlesHandler);
