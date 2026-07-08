418c8cd956d882b49d63093811d9f91a9bdc12f6
Chayney <chunchunmaru33333@gmail.com>
2026-07-09 01:51:14 +0900

    バックエンドの構築

--- backend/node/src/controller/trend.controller.ts
+++ backend/node/src/controller/trend.controller.ts
@@ -13,8 +13,7 @@ export const getTrendArticlesHandler: RequestHandler = async (_req, res) => {
             },
             order: {
                 likes_count: "DESC"
-            },
-            
+            },       
         });
 
         res.json(trendArticles);
