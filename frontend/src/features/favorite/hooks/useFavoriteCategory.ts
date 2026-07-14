00559479ad34160406a68bd846b727b2e7f967ac
Chayney <chunchunmaru33333@gmail.com>
2026-07-14 21:59:42 +0900

    フロントエンドの構築

--- frontend/src/features/favorite/hooks/useFavoriteCategory.ts
+++ frontend/src/features/favorite/hooks/useFavoriteCategory.ts
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import type { Category } from "../types/favorite";
 import { API_URL } from "../../../shared/api/apiClient";
+import { getAccessToken, onAuthChange } from "../../../shared/api/supabaseClient";
 
 export const useFavoriteCategory = () => {
     const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const res = await fetch(`${API_URL}/categories`);
+            const token = await getAccessToken();
+            const res = await fetch(`${API_URL}/categories`, {
+                headers: token ? { Authorization: `Bearer ${token}`} : {}
+            });
 
             const json: Category[] = await res.json();
             
@@ -15,6 +19,13 @@ export const useFavoriteCategory = () => {
         };
 
         fetchData();
+
+        // ログイン状態を監視
+        const unsubscribe = onAuthChange(() => {
+            fetchData();
+        });
+
+        return unsubscribe;
     }, []);
     
     return {
--- frontend/src/shared/api/supabaseClient.ts
+++ frontend/src/shared/api/supabaseClient.ts
@@ -1,8 +1,18 @@
 // supabaseのaccess_tokenをNode.jsで検証
 import { supabase } from "../lib/supabaseClient";
 
-export async function getAccessToken() {
+export const getAccessToken = async () => {
     const { data: { session } } = await supabase.auth.getSession();
 
     return session?.access_token;
+}
+
+export const onAuthChange = (callback: () => void) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(() => {
+        callback();
+    });
+
+    return () => {
+        subscription.unsubscribe();
+    };
 }
